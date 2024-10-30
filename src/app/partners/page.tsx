a41902c3dd29b31f2668ea367e72de547c02049c
Ioanna Papaioannou <ipapioani@gmail.com>
2024-10-30 23:06:10 +0100

    styling links experimenting text

--- src/app/partners/page.tsx
+++ src/app/partners/page.tsx
@@ -8,30 +8,45 @@ const PartnersPage: FC = () => {
       <h1 className="section-title">The Partners</h1>
 
       <article className="partner">
-        <h2 className="partner-title">
-          University of Patras, Department of Civil Engineering
-        </h2>
         <p>
-          Founded in 1972, the Department of Civil Engineering at the University
-          of Patras is the host institution of the project. The department
-          consists of:
+          Founded in 1972, the{" "}
+          <Link
+            href="https://www.civil.upatras.gr/?lang=en"
+            target="_blank"
+            className="partner-link"
+          >
+            Department of Civil Engineering
+          </Link>{" "}
+          at the{" "}
+          <Link
+            href="https://www.civil.upatras.gr/?lang=en"
+            target="_blank"
+            className="partner-link"
+          >
+            University of Patras
+          </Link>{" "}
+          is the host institution of the project. The department consists of
+          three divisions, nine laboratories, a Seismic Simulator Facility, a
+          Fire Testing Facility, and two Computer Centers covering the areas of
         </p>
         <ul>
-          <li>Three Divisions</li>
-          <li>Nine Laboratories</li>
-          <li>A Seismic Simulator Facility</li>
-          <li>A Fire Testing Facility</li>
-          <li>Two Computer Centers</li>
-        </ul>
-        <p>
-          The department covers Structural, Geotechnical, Hydraulic,
-          Environmental, and Transportation Engineering. It also specializes in:
-        </p>
-        <ul>
+          <li>Structural Engineering</li>
+          <li>Geotechnical Engineering and Hydraulic Engineering</li>
+          <li>Environmental Engineering and Transportation Engineering</li>
+          <ul />
           <li>
             Research and development of innovative systems for combined energy
             and seismic retrofitting of buildings
           </li>
+          <p>
+            {" "}
+            Among its notable expertise, the department focuses on the research
+            and development of innovative systems for combined energy and
+            seismic retrofitting of buildings. It also excels in numerical
+            methods for assessing dynamic energy performance and urban
+            microclimate analysis, along with hygrothermal performance and
+            sustainable design of the urban built environment.
+          </p>
           <li>
             Numerical methods for dynamic energy performance of buildings and
             urban microclimate analysis
@@ -40,46 +55,25 @@ const PartnersPage: FC = () => {
             Hygrothermal performance of buildings and sustainable urban design
           </li>
         </ul>
-        <p>Learn more about the University of Patras:</p>
-        <Link
-          href="https://www.civil.upatras.gr/?lang=en"
-          target="_blank"
-          className="partner-link"
-        >
-          University of Patras Civil Engineering Department
-        </Link>
-        <br />
-        <Link
-          href="https://www.upatras.gr/en/"
-          target="_blank"
-          className="partner-link"
-        >
-          University of Patras Official Website
-        </Link>
       </article>
 
       <article className="partner">
-        <h2 className="partner-title">
-          National Technical University of Athens (NTUA)
-        </h2>
         <p>
-          The National Technical University of Athens (NTUA) is a collaborating
-          organization for the Re.Nature Cities project. NTUA participates
-          through the School of Mechanical Engineering, Fluids Section,
-          contributing by:
+          The{" "}
+          <Link
+            href="http://wt.fluid.mech.ntua.gr/"
+            target="_blank"
+            className="partner-link"
+          >
+            National Technical University of Athens (NTUA){" "}
+          </Link>{" "}
+          is a collaborating organization for the Re.Nature Cities project. NTUA
+          participates through the School of Mechanical Engineering, Fluids is a
+          Collaborating Organization of the Re.Nature Cities project. NTUA
+          participates through the School of Mechanical Engineering, Fluids
+          Section and assures the wind tunnel measurements of the drag
+          coefficient of common urban tree species
         </p>
-        <ul>
-          <li>Conducting wind tunnel measurements</li>
-          <li>Studying the drag coefficient of common urban tree species</li>
-        </ul>
-        <p>Learn more about NTUA's contribution:</p>
-        <Link
-          href="http://wt.fluid.mech.ntua.gr/"
-          target="_blank"
-          className="partner-link"
-        >
-          NTUA Fluids Section
-        </Link>
       </article>
     </section>
   );
