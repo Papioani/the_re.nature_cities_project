7b85edbfeba80cee27390f9bb8b9005392d18be1
Ioanna Papaioannou <ipapioani@gmail.com>
2024-11-07 14:21:31 +0100

    contact

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -1,16 +1,22 @@
 // src/app/components/Footer.tsx
 import React from "react";
+import Link from "next/link";
 
 const Footer: React.FC = () => {
   return (
     <footer>
-      <div className="footerImages">
-        <img src="/elidek.png" alt="Elidek logo" className="footerImage" />
-        <img
-          src="/Ελλάδα2.0.jpg"
-          alt="image of Greece2.0"
-          className="footerImage"
-        />
+      <div className="footerLinks">
+        <div className="footerImages">
+          <Link href="/contact" className="footer-link">
+            Contact
+          </Link>
+          <img src="/elidek.png" alt="Elidek logo" className="footerImage" />
+          <img
+            src="/Ελλάδα2.0.jpg"
+            alt="image of Greece2.0"
+            className="footerImage"
+          />
+        </div>
       </div>
     </footer>
   );
