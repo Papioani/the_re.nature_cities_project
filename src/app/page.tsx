9df0dad8e18ba241d677f18d28015083b01caf00
Ioanna Papaioannou <ipapioani@gmail.com>
2024-11-09 18:48:51 +0100

    lines Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 export default function Home() {
   return (
     <div className="flex flex-col">
-      <main className="flex flex-col gap-8 items-start sm:items-start p-8 pb-20">
-        <div className="centered-container">
-          <h1 className="centered-heading"> Welcome to Re.Nature Cities</h1>
+      <main className="flex flex-col gap-4 items-start sm:items-start p-8 pb-10">
+        <div className="w-full flex justify-center items-start gap-4">
+          <div className="line"></div> {/* Line on the left */}
+          <h1 className="centered-heading flex flex-col items-center text-center">
+            {" "}
+            Welcome to Re.Nature Cities
+          </h1>
+          <div className="line"></div> {/* Line on the right */}
         </div>
+
         <p className="text-lg leading-relaxed mb-6 border-l-4 border-[#059669] pl-4">
           This research project focuses on experimental and numerical methods to
           assess the role of street trees as a Nature-Based Solution for climate
--- src/app/the-re.nature-cities-project/page.tsx
+++ src/app/the-re.nature-cities-project/page.tsx
@@ -7,7 +7,7 @@ import React, { FC } from "react";
 const ReNaturePage: FC = () => {
   return (
     <section className="re.NatureProjectContent px-4 py-2 pb-16 ">
-      <h2>The Scope</h2>{" "}
+      <h2 className="marker-square">The Scope</h2>{" "}
       <p className="text-lg leading-relaxed mb-6 border-l-4 border-[#059669] pl-4">
         Re.Nature Cities aims to evaluate via experimental and simulation means
         the ability of street trees as nature-based solutions (NBS) to act as an
@@ -17,7 +17,7 @@ const ReNaturePage: FC = () => {
         well-being of the citizens.
       </p>{" "}
       <div className="section-divider"></div>
-      <h2>The Objectives</h2>
+      <h2 className="marker-square">The Objectives</h2>
       <ul className="list-custom pl-6">
         <li className="mb-2">
           To assess the{" "}
