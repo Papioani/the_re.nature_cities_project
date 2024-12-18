46a3aefdcd41961828c16a650bb76c3368a74a30
Ioanna Papaioannou <ipapioani@gmail.com>
2024-12-19 00:56:59 +0100

    add right margin navlink action

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,18 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    // Paths to your pages, components, and other relevant files in the src directory
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/deliverables/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/fonts/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/partners/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/project-outline/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/styles/**/*.{css}", // Include any global styles
+    "./src/app/the-action/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/the-re.nature/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/utils/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/wind-tunnel/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
