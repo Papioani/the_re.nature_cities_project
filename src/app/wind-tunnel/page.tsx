5ab8dd8e0ab5cc85e3f5e297b37312d77a4db657
Ioanna Papaioannou <ipapioani@gmail.com>
2024-12-30 20:54:13 +0100

    add wind-tunnel info

--- src/app/wind-tunnel/page.tsx
+++ src/app/wind-tunnel/page.tsx
@@ -1,7 +1,59 @@
-import React from "react";
+"use client";
+import React, { useEffect, useRef } from "react";
+import styles from "./Wind-tunnel.module.css";
+import Image from "next/image";
 
-const ComponentName: React.FC = () => {
-  return <div>- Deliverables - Publications / Conference announcements</div>;
+const WindTunnel: React.FC = () => {
+  const mainContentRef = useRef<HTMLElement>(null);
+  const firstParagraphRef = useRef<HTMLParagraphElement>(null);
+
+  // Set focus to the main content on load
+  useEffect(() => {
+    if (mainContentRef.current) {
+      mainContentRef.current.focus({ preventScroll: true });
+    }
+    if (firstParagraphRef.current) {
+      firstParagraphRef.current.focus({ preventScroll: true });
+    }
+  }, []);
+  return (
+    <section
+      className=" text-m px-6 pb-16"
+      ref={mainContentRef}
+      aria-labelledby="WindTunnelTitle"
+    >
+      <h2>Wind Tunnel, LAI/LAD & Albedo Measurements</h2>
+      <div className="section-divider"></div>
+      <h3 id="NTUA" className={styles.NTUAPackageTitle}>
+        NTUA Wind tunnel experiments{" "}
+      </h3>
+      <div className={styles.NTUADescription}>
+        <article className={styles.NTUA}>
+          <ul>
+            <p>
+              Experimental determination of the drag coefficient (Cd) of natural
+              trees/shrubs with different geometric properties
+            </p>
+            <p>
+              Representative deciduous and evergreen species, commonly used in
+              urban areas of Mediterranean region, i.e. i) Clementine, ii)
+              Magnolia, iii) Oleander and iv) Tilia.
+            </p>
+            <p> Trees samples’height: 1.8m</p>
+            <p>Examined wind speeds: 1.4m/s-10m/s</p>
+            <p>3 examined samples of each tree type</p>
+          </ul>
+
+          <Image
+            src="/WIND TUN.jpg"
+            alt="Photo of the tree sample"
+            width={500}
+            height={300}
+            className={styles.treePhoto}
+          />
+        </article>
+      </div>
+    </section>
+  );
 };
-
-export default ComponentName;
+export default WindTunnel;
