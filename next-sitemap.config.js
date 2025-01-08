38b2fa32179b0beea7f011e5da605cd0c37be713
Ioanna Papaioannou <ipapioani@gmail.com>
2025-01-08 18:09:30 +0100

    fix siteUrl bug

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  siteUrl: "https://www.yourdomain.com", // Replace with your actual domain
+  siteUrl: "https://www.renaturecities.com", // Replace with your actual domain
   generateRobotsTxt: true, // (Optional) Generate robots.txt file
 };
