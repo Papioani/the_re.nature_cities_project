ce9f1d72c6d8358c9911431b0eeadf37b54e51c4
Ioanna Papaioannou <ipapioani@gmail.com>
2025-01-08 15:03:23 +0100

    details

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,7 +32,7 @@ const Home: React.FC = () => {
           by the European Union - NextGenerationEU (H.F.R.I. Project Number:
           15566).
         </p>
-        <div className="section-divider"></div>
+        <div className="section-divider pb-0"></div>
         <div className=" leading-relaxed mb-6 text-left">
           <p className="markerSquare mb-1">
             <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400 font-bold">
