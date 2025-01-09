46055f05f48ff14d5dcc9f0fc33b002fac37aeac
Ioanna Papaioannou <ipapioani@gmail.com>
2025-01-09 02:39:24 +0100

    add favicons and manifest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,26 @@ export default function RootLayout({
       <head>
         {/* Add the viewport meta tag here */}
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/Logo2.png" type="image/png" />
+        {/* Favicon and Manifest */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="shortcut icon" href="/favicon.ico" />
       </head>
       <body className="antialiased flex flex-col min-h-screen w-full">
         {/*  <ScrollHandler /> */}
