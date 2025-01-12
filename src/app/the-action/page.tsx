dab68dc5a31ce1117662b80160e9c3bbf6bd315e
Ioanna Papaioannou <ipapioani@gmail.com>
2025-01-12 17:40:59 +0100

    fix double the action

--- src/app/the-action/page.tsx
+++ src/app/the-action/page.tsx
@@ -7,20 +7,20 @@ const ActionPage: React.FC = () => {
 
       <div className="action-description text-justify text-lg space-y-4 p-4 shadow-lg rounded-lg">
         <p>
-          The action The Action “Basic Research Financing (Horizontal support
-          for all Sciences)” (ID 16618 - Subproject 1) is included in the
-          projects of component 4.5 “Promote Research and Innovation” within the
-          framework of the National Recovery and Resilience Plan “Greece 2.0”
-          funded by the European Union - Next Generation EU and aims to
-          contribute to the achievement of the general goals set by the National
-          Recovery and Resilience Plan “Greece 2.0” (hereinafter “Greece 2.0”).
-          Component 4.5 “Promote Research and Innovation”, which includes the
-          present Action, aims to increase public and private investments for
-          research and development (R&amp;D), strengthen connections between
-          science and businesses and develop innovative infrastructures for
-          R&amp;D. This will be achieved through a series of projects, as well
-          as reforms, aiming to motivate innovative businesses to invest in the
-          R&amp;D sector. Sub-action 1, in which RE.Nature Cities Project is
+          The Action “Basic Research Financing (Horizontal support for all
+          Sciences)” (ID 16618 - Subproject 1) is included in the projects of
+          component 4.5 “Promote Research and Innovation” within the framework
+          of the National Recovery and Resilience Plan “Greece 2.0” funded by
+          the European Union - Next Generation EU and aims to contribute to the
+          achievement of the general goals set by the National Recovery and
+          Resilience Plan “Greece 2.0” (hereinafter “Greece 2.0”). Component 4.5
+          “Promote Research and Innovation”, which includes the present Action,
+          aims to increase public and private investments for research and
+          development (R&amp;D), strengthen connections between science and
+          businesses and develop innovative infrastructures for R&amp;D. This
+          will be achieved through a series of projects, as well as reforms,
+          aiming to motivate innovative businesses to invest in the R&amp;D
+          sector. Sub-action 1, in which RE.Nature Cities Project is
           implemented, aims to finance projects with Principal Investigators
           (PI) who are newly appointed or are currently undergoing an
           appointment procedure for a non-permanent position (on a term basis)
