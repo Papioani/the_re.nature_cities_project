05ccb9a29d5f609f748041d7763eee62862e72c0
Ioanna Papaioannou <ipapioani@gmail.com>
2025-02-01 20:19:26 +0100

    restore NEXT_PUBLIC_GOOGLE_API_KEY

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
           sizes="16x16"
           href="/favicon-16x16.png"
         />
-        {/* <link rel="manifest" href="/site.webmanifest" /> */}
+        <link rel="manifest" href="/site.webmanifest" />
         <link rel="shortcut icon" href="/favicon.ico" />
       </head>
       <body className="antialiased flex flex-col min-h-screen w-full">
