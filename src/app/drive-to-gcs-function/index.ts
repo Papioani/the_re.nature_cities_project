9e4026b15a62cb7f7ae09538fefaa035d7f81e05
Ioanna Papaioannou <ipapioani@gmail.com>
2025-02-28 21:55:17 +0100

    Setup Cloud Function for Google Drive to Cloud Storage upload

--- src/app/drive-to-gcs-function/index.ts
+++ src/app/drive-to-gcs-function/index.ts
@@ -1,23 +1,19 @@
-// index.js
+// index.ts
 import { google } from "googleapis";
 import { Storage as GCSStorage } from "@google-cloud/storage";
-import * as path from "path";
 import { Request, Response } from "express";
 import * as dotenv from "dotenv";
 
 dotenv.config();
 
-// Set the path to your JSON key file
-const keyFilePath = path.join(
-  __dirname,
-  process.env.GOOGLE_APPLICATION_CREDENTIALS || ""
-); // Place the JSON key in the same folder (or adjust path)
-
 // Initialize Google Drive API Client
 const drive = google.drive("v3");
+
 // Initialize Google Cloud GCSStorage Client
 const storage = new GCSStorage();
-const bucketName = process.env.GCS_BUCKET_NAME || "default-bucket-name";
+const bucketName =
+  process.env.GCS_BUCKET_NAME ||
+  "gcf-v2-uploads-599364547661.us-central1.cloudfunctions.appspot.com";
 const bucket = storage.bucket(bucketName);
 
 /**
@@ -41,26 +37,26 @@ export const uploadToCloudStorage = async (req: Request, res: Response) => {
   // Authenticate using the service account JSON key
   try {
     const auth = new google.auth.GoogleAuth({
-      keyFile: keyFilePath,
+      keyFile: process.env.GOOGLE_APPLICATION_CREDENTIALS || undefined,
       scopes: [
         "https://www.googleapis.com/auth/drive.readonly",
         "https://www.googleapis.com/auth/devstorage.read_write",
       ],
     });
-    google.options({ auth: auth });
 
     // Fetch the file from Google Drive as a stream
     const driveResponse = await drive.files.get(
       {
         fileId,
         alt: "media",
+        auth,
       },
       { responseType: "stream" }
     );
 
     // Define the destination file name in Cloud Storage
-    const destFileName = `drive_uploads/${fileId}.pdf`; // Customize as needed
-    const cloudFile = bucket.file(destFileName);
+    const destFileName = `drive_uploads/${fileId}.pdf`; //  the new name for the file once it's uploaded to Cloud Storage.
+    const cloudFile = bucket.file(destFileName); //  the new name for the file in Cloud Storage
 
     // Create a write stream for Cloud Storage
     const writeStream = cloudFile.createWriteStream({
